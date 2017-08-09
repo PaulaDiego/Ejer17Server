34ce6fd3173e35bc1d2ede48fe0e587bc6a24c5c
PaulaDiego <paulacursocic@gmail.com>
2017-08-09 18:32:20 +0200

    commit persona

---
+++ Ejer17Server/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejer17Server
+{
+    public class Persona
+    {
+        public String Nombre {get;set;}
+        public String Apellidos { get; set; }
+        public int Edad { get; set; }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(String _Nombre,String _Apellidos,int _Edad)
+        {
+            this.Nombre = _Nombre;
+            this.Apellidos = _Apellidos;
+            this.Edad = _Edad;
+        }
+    }
+}
